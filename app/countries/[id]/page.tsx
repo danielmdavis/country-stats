7ea4a5464c8d7d4b78bf5b17d283122860f18a24
danielmdavis <danis1911@gmail.com>
2023-02-20 12:44:04 -0500

    big flag

--- app/countries/[id]/page.tsx
+++ app/countries/[id]/page.tsx
@@ -6,15 +6,22 @@ const data = require('./../../../countries.json')
 export default function CountryView(context: any) {
   let i: any
   const country = data.find((i: any) => i.id == context.params.id)
+  let curSymbol: string = ''
+  let curName: string = ''
 
   return (
     <div style={{ margin: 'auto', display: 'block' }}>
       <br />
-      <div style={{ border: '4px solid lime', width: '400px', margin: '10px', padding: '12px', backgroundColor: 'lightskyblue' }}>
-        <h1>{country?.name} {country?.emoji}</h1>
-        <div>capital: {country?.capital}</div>
-        <div>region: {country?.region}</div>
-        <div>currency: {country?.currency_name} (<span style={{fontSize: '1.2em'}}>{country?.currency_symbol}</span>)</div>
+      <div style={{ border: '4px solid lime', width: '400px', margin: '10px', padding: '12px', backgroundColor: 'lightskyblue', display: 'flex', flexDirection: 'row' }}>
+        <div style={{ flexDirection: 'column', margin: '20px' }}>
+          <h1>{country?.name} </h1>
+          <div>capital: {country?.capital}</div>
+          <div>region: {country?.region}</div>
+          <div>currency: {curName} (<span style={{fontSize: '1.2em'}}>{curSymbol}</span>)</div>
+        </div>
+        <div style={{ fontSize: '7.5em', margin: '20px' }}>
+          {country?.emoji}
+        </div>
       </div>
     </div>
   )
