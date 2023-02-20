29bd3f6f62d08c40616780b8d5ee2d7238a26480
danielmdavis <danis1911@gmail.com>
2023-02-20 12:18:08 -0500

    typology

--- app/countries/[id]/page.tsx
+++ app/countries/[id]/page.tsx
@@ -11,10 +11,10 @@ export default function CountryView(context: any) {
     <div style={{ margin: 'auto', display: 'block' }}>
       <br />
       <div style={{ border: '4px solid lime', width: '400px', margin: '10px', padding: '12px', backgroundColor: 'lightskyblue' }}>
-        <h1>{country.name} {country.emoji}</h1>
-        <div>capital: {country.capital}</div>
-        <div>region: {country.region}</div>
-        <div>currency: {country.currency_name} (<span style={{fontSize: '1.2em'}}>{country.currency_symbol}</span>)</div>
+        <h1>{country?.name} {country?.emoji}</h1>
+        <div>capital: {country?.capital}</div>
+        <div>region: {country?.region}</div>
+        <div>currency: {country?.currency_name} (<span style={{fontSize: '1.2em'}}>{country.currency_symbol}</span>)</div>
       </div>
     </div>
   )
