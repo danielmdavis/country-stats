3494a87914c930602e5172ac81360700edab8653
danielmdavis <danis1911@gmail.com>
2023-02-20 12:05:10 -0500

    routing

--- app/countries/[id]/page.tsx
+++ app/countries/[id]/page.tsx
@@ -5,17 +5,16 @@ const data = require('./../../../countries.json')
 
 export default function CountryView(context: any) {
   const country = data.find(i => i.id == context.params.id)
-  console.log(data)
 
   return (
     <div style={{ margin: 'auto', display: 'block' }}>
-      {/* <br />
+      <br />
       <div style={{ border: '4px solid lime', width: '400px', margin: '10px', padding: '12px', backgroundColor: 'lightskyblue' }}>
         <h1>{country.name} {country.emoji}</h1>
         <div>capital: {country.capital}</div>
         <div>region: {country.region}</div>
         <div>currency: {country.currency_name} (<span style={{fontSize: '1.2em'}}>{country.currency_symbol}</span>)</div>
-      </div> */}
+      </div>
     </div>
   )
 }
